ce673279fb50fae7fb2e827e4b0126123da1b51c
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-23 20:02:59 +0000

    add categorys

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project3/model/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +11,33 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.66,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Rice',
+      amount: 18.71,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Car',
+      amount: 1000,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
   @override
   Widget build(context) {
     return const Scaffold(

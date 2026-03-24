6e275a5a742f31a5710ce0ed83bfb1dee64ad6f4
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-24 21:35:03 +0000

    structuring the project files

---
+++ lib/expenses_list/expenses_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:project3/model/expense.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(children: [Text('\$${expense.amount.toStringAsFixed(2)}')]),
+          ],
+        ),
+      ),
+    );
+  }
+}

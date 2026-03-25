b35988e26de2ed16889a34cceea08604721da801
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-25 19:55:02 +0000

    add the app bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -39,9 +39,25 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Modal bottom sheet'),
+    );
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),

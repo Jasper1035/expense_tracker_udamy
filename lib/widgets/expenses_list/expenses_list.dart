e62538dae3056ac09f1ad11765702f844007b608
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-29 18:33:59 +0100

    add dismissable widget

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,26 @@ import 'package:project3/model/expense.dart';
 import 'package:project3/widgets/expenses_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        key: ValueKey(expenses[index]),
+        child: ExpensesItem(expenses[index]),
+      ),
     );
   }
 }

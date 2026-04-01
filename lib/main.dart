4ae03303a10a7b1fcca7fb660ebd70cefaab27ae
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-04-01 21:08:10 +0100

    changes besic themes of the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:project3/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 255, 0, 0),
+);
+
 void main() {
   runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: Expenses()),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.red,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Expenses(),
+    ),
   );
 }

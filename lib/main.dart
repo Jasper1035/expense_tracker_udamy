45d3ab641dfcc947925cd693d4e42400035c204e
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-04-03 21:14:26 +0100

    add the dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 255, 0, 0),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -24,8 +42,16 @@ void main() {
             backgroundColor: kColorScheme.primaryContainer,
           ),
         ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+            color: Colors.black,
+          ),
+        ),
       ),
 
+      // themeMode: ThemeMode.light,  // by default is is set to system theme
       debugShowCheckedModeBanner: false,
       home: const Expenses(),
     ),
